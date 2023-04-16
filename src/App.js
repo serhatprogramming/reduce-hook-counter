a3f9c6c39f3835663e8f46207cc8ade53d68a6af
serhatprogramming <serhatprogramming@gmail.com>
2023-04-16 15:27:08 -0400

    ContextProvider wrap in index.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import { useReducer } from "react";
-
-import CounterContext from "./CounterContext";
-
 import Display from "./components/Display";
 import Button from "./components/Button";
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    case "DEC":
-      return state - 1;
-    case "RES":
-      return 0;
-
-    default:
-      return state;
-  }
-};
-
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0);
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <div>
-        <Display />
-        <Button label="-" type="DEC" />
-        <Button label="reset" type="RES" />
-        <Button label="+" type="INC" />
-      </div>
-    </CounterContext.Provider>
+    <div>
+      <Display />
+      <Button label="-" type="DEC" />
+      <Button label="reset" type="RES" />
+      <Button label="+" type="INC" />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { CounterContextProvider } from "./CounterContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+);
