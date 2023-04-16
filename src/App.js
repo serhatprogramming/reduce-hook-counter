75997ae7db1bef2cbd083bc00f483f07530fd53c
serhatprogramming <serhatprogramming@gmail.com>
2023-04-16 14:38:23 -0400

    reducer added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div>Test</div>;
-}
+import { useReducer } from "react";
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "RES":
+      return 0;
+
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [counter, dispatchCounter] = useReducer(counterReducer, 0);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={() => dispatchCounter({ type: "DEC" })}>-</button>
+      <button onClick={() => dispatchCounter({ type: "RES" })}>reset</button>
+      <button onClick={() => dispatchCounter({ type: "INC" })}>+</button>
+    </div>
+  );
+};
 
 export default App;
